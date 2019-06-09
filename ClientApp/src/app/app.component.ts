41c25d276b37b752c0a1e3abc2d9742f1a72e918
FoxGeoff <fox.geoffrey@gmail.com>
2019-06-09 16:22:51 -0700

    Cleanup and Bootstrap 4.x

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Repository } from './models/repository';
+import { Movie } from './models/movie.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Web Development with ASP.NET & Angular 7';
+
+  constructor(private repro: Repository){}
+
+  get movie() : Movie {
+    return  this.repro.movie;
+  }
 }
